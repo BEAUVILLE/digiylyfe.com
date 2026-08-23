585fd875fac08a726ad2fbe3a85cce87e5093dc8
DIGIY LYFE <baptistejb24@gmail.com>
2026-08-23 23:56:55 +0000

    add isolated CARNET master test service worker

---
+++ carnet-master-test/sw.js
@@ -0,0 +1,42 @@
+const CACHE = "digiy-carnet-master-test-v1-20260823-a";
+const CORE = [
+  "./",
+  "./index.html",
+  "./master-config.js",
+  "./manifest.webmanifest",
+  "./app/hub.html",
+  "./app/index.html",
+  "./app/oreille.html",
+  "./app/dettes.html",
+  "./assets/carnet-contract.js",
+  "./assets/carnet-store.js",
+  "./assets/carnet-oreille.js",
+  "./assets/carnet-debts.js",
+  "./assets/carnet-i18n.js",
+  "./assets/carnet-world8-ui.js"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(CORE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith("digiy-carnet-master-test-") && key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  if(event.request.method !== "GET") return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy)).catch(()=>{});
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(hit => hit || caches.match("./index.html")))
+  );
+});
