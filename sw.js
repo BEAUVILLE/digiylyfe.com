885d5be9cd7a014abf4bd625aa0d83e894b00c24
DIGIY LYFE <baptistejb24@gmail.com>
2026-08-24 02:01:35 +0000

    PWA: mark PRO CARNET showcase release 20260824

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    Installation permanente, sans cache de page et sans réécriture de route.
 */
 
-const DIGIY_PWA_VERSION = 'digiylyfe-pwa-20260810';
+const DIGIY_PWA_VERSION = 'digiylyfe-pwa-20260824-pro-carnet-vitrine';
 
 self.addEventListener('install', () => {
   self.skipWaiting();
